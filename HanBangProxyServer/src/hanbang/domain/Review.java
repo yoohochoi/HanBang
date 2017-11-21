b4abd61f535e321d9d1790c4134e6aa7861ad7e1
yoohochoi <yoohochoi@naver.com>
2017-11-21 15:35:36 +0900

    proxyServer create

---
+++ HanBangProxyServer/src/hanbang/domain/Review.java
@@ -0,0 +1,74 @@
+package hanbang.domain;
+
+import java.util.List;
+
+public class Review {
+	
+	private int reviewId;
+	private String writerId;
+	private int date;
+	private String title;
+	private String photo;
+	private String content;
+	private List<Answer> answers;
+	
+	public Review() {
+	}
+
+	public int getReviewId() {
+		return reviewId;
+	}
+
+	public void setReviewId(int reviewId) {
+		this.reviewId = reviewId;
+	}
+
+	public String getWriterId() {
+		return writerId;
+	}
+
+	public void setWriterId(String writerId) {
+		this.writerId = writerId;
+	}
+
+	public int getDate() {
+		return date;
+	}
+
+	public void setDate(int date) {
+		this.date = date;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public List<Answer> getAnswers() {
+		return answers;
+	}
+
+	public void setAnswers(List<Answer> answers) {
+		this.answers = answers;
+	}
+	
+}
