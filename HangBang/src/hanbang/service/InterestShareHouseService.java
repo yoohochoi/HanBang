f12ce4af99ef72e81f9e60a97fac312fc2c42c3d
수현 <mynamessu520@gmail.com>
2017-11-21 14:08:18 +0900

    InterestShareHouseSerice수정

--- HangBang/src/hanbang/service/InterestShareHouseService.java
+++ HangBang/src/hanbang/service/InterestShareHouseService.java
@@ -8,7 +8,8 @@ public interface InterestShareHouseService {
 	
 	boolean register(String memberId, int shareHouseId);
 	List<ShareHouse> findAll(String memberId);
-	boolean remove(String memberId);
-	boolean removeByShareHouse(int shareHouseId);
+	boolean remove(int shareHouseId);
+	boolean removeByMemberId(String memberId);
+	boolean removeInterestShareHouse(String memberId, int shareHouseId);
 
 }
