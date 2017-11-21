7da79680de887acea194fd7d6febe854b8cded19
yoohochoi <yoohochoi@naver.com>
2017-11-21 10:13:46 +0900

    Member DAO create

---
+++ HanBang/src/domain/Member.java
@@ -0,0 +1,41 @@
+package domain;
+
+public class Member {
+	private String memberId;
+	private String password;
+	private String name;
+	private int phoneNumber;
+
+	public String getMemberId() {
+		return memberId;
+	}
+
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+}
