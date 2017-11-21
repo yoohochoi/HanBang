88d04b95dc1e589f7482bf1300d239e15e78ec5c
수현 <mynamessu520@gmail.com>
2017-11-21 14:40:05 +0900

    Controller 폼 추가

---
+++ HangBang/src/hanbang/controller/MemberController.java
@@ -0,0 +1,16 @@
+package hanbang.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import hanbang.service.MemberService;
+
+@Controller
+public class MemberController {
+
+	@Autowired
+	private MemberService service;
+	
+	
+	
+}
