cf6c1513dba181761c15bb0dc09f8e31a9225e4b
Limsora <th_fkc@naver.com>
2017-11-22 17:17:59 +0900

    1122

--- HangBang/src/hanbang/service/logic/HouseServiceLogic.java
+++ HangBang/src/hanbang/service/logic/HouseServiceLogic.java
@@ -1,41 +1,77 @@
 package hanbang.service.logic;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import hanbang.domain.House;
 import hanbang.service.HouseService;
+import hanbang.store.HouseStore;
+import hanbang.store.logic.HouseStoreLogic;
 
 @Service
-public class HouseServiceLogic implements HouseService{
+public class HouseServiceLogic implements HouseService {
+
+	private HouseStore houseStore;
+	private int check;
+
+	public HouseServiceLogic() {
+		houseStore = new HouseStoreLogic();
+	}
 
 	@Override
 	public boolean register(House house) {
-		// TODO Auto-generated method stub
-		return false;
+		check = houseStore.create(house);
+
+		if (check > 0) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	@Override
 	public House find(int houseId) {
-		// TODO Auto-generated method stub
-		return null;
+		return houseStore.retrive(houseId);
+	}
+
+	@Override
+	public List<House> findByMemberId(String memberId) {
+
+		return houseStore.retriveByMemberId(memberId);
 	}
 
 	@Override
 	public boolean modify(House house) {
-		// TODO Auto-generated method stub
-		return false;
+		check = houseStore.update(house);
+
+		if (check > 0) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	@Override
 	public boolean remove(int houseId) {
-		// TODO Auto-generated method stub
-		return false;
+		check = houseStore.delete(houseId);
+
+		if (check > 0) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	@Override
 	public boolean removeByMemberId(String memberId) {
-		// TODO Auto-generated method stub
-		return false;
+		check = houseStore.deleteByMemberId(memberId);
+
+		if (check > 0) {
+			return true;
+		} else {
+			return false;
+		}
 	}
-	
+
 }
--- HangBang/src/hanbang/service/logic/MemberServiceLogic.java
+++ HangBang/src/hanbang/service/logic/MemberServiceLogic.java
@@ -4,45 +4,83 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import hanbang.domain.House;
 import hanbang.domain.Member;
+import hanbang.domain.ShareHouse;
 import hanbang.service.MemberService;
+import hanbang.store.HouseStore;
+import hanbang.store.MemberStore;
+import hanbang.store.ReviewStore;
+import hanbang.store.ShareHouseStore;
+import hanbang.store.logic.HouseStoreLogic;
+import hanbang.store.logic.MemberStoreLogic;
+import hanbang.store.logic.ReviewStoreLogic;
+import hanbang.store.logic.ShareHouseStoreLogic;
 
 @Service
-public class MemberServiceLogic implements MemberService{
+public class MemberServiceLogic implements MemberService {
+
+	private MemberStore memberStore;
+	private HouseStore houseStore;
+	private ShareHouseStore shareHouseStore;
+	private ReviewStore reviewStore;
+
+	private int check;
+
+	public MemberServiceLogic() {
+		memberStore = new MemberStoreLogic();
+		houseStore = new HouseStoreLogic();
+		shareHouseStore = new ShareHouseStoreLogic();
+		reviewStore = new ReviewStoreLogic();
+	}
 
 	@Override
 	public boolean register(Member member) {
-		// TODO Auto-generated method stub
-		return false;
+
+		check = memberStore.create(member);
+
+		if (check > 0) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	@Override
 	public List<Member> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+	
+		return memberStore.retriveAll();
 	}
 
 	@Override
 	public Member find(String memberId) {
-		// TODO Auto-generated method stub
-		return null;
+
+		return memberStore.retrive(memberId);
 	}
 
 	@Override
 	public boolean modify(Member member) {
-		// TODO Auto-generated method stub
-		return false;
+		check = memberStore.update(member);
+		if (check > 0) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	@Override
 	public boolean remove(String memberId) {
-		// TODO Auto-generated method stub
-		return false;
+		check = memberStore.delete(memberId);
+		if (check > 0) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	@Override
 	public boolean login(String memberId, String password) {
-		// TODO Auto-generated method stub
+
 		return false;
 	}
 
