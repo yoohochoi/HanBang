711c040aacf488f1f78e2e8053fb0909d7565b48
Limsora <th_fkc@naver.com>
2017-11-24 16:28:31 +0900

    dd

---
+++ HangBang/test/hanbang/sr/MemberStoreLogicTest.java
@@ -0,0 +1,63 @@
+package hanbang.sr;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import hanbang.domain.Member;
+import hanbang.store.MemberStore;
+import hanbang.store.logic.MemberStoreLogic;
+
+public class MemberStoreLogicTest {
+	
+	private MemberStore store;
+	
+	@Before
+	public void setUP() {
+		store = new MemberStoreLogic();
+	}
+
+//	@Test
+//	public void testCreate() {
+//		Member member = new Member();
+//		member.setId("sora525");
+//		member.setPassword("1525");
+//		member.setName("ss");
+//		member.setPhoneNumber(01055332);
+//		member.setEmail("th_fkcdsc@nate.com");
+//		member.setMemberTypeId(2);
+//		store.create(member);
+//		assertNotNull(member);
+//	}
+//
+//	@Test
+//	public void testRetriveAll() {
+//		List<Member> list = store.retriveAll();
+//		assertEquals(2, list.size());
+//	}
+//
+//	@Test
+//	public void testRetrive() {
+//		Member member = store.retrive("sora525");
+//		
+//		assertEquals("ss", member.getName());
+//		
+//	}
+
+	@Test
+	public void testUpdate() {
+		Member member = store.retrive("sora525");
+		member.setPhoneNumber(546);
+		store.update(member);
+	}
+
+//	@Test
+//	public void testDelete() {
+//		store.delete("sora525");
+//		
+//	}
+
+}
