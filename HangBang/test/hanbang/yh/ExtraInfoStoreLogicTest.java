906545d0f03a85040aa0277b707a7f8a1bf5ae67
yoohochoi <yoohochoi@naver.com>
2017-11-24 09:43:18 +0900

    ExtraInfoStoreLogic test

---
+++ HangBang/test/hanbang/yh/ExtraInfoStoreLogicTest.java
@@ -0,0 +1,40 @@
+package hanbang.yh;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import hanbang.store.ExtraInfoStore;
+import hanbang.store.logic.ExtraInfoStoreLogic;
+
+public class ExtraInfoStoreLogicTest {
+
+	private ExtraInfoStore extraInfoStore;
+
+	@Before
+	public void setUp() {
+		extraInfoStore = new ExtraInfoStoreLogic();
+	}
+
+//	@Test
+//	public void testCreate() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testRetrive() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testUpdate() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testDeleteByShareHouse() {
+//		fail("Not yet implemented");
+//	}
+
+}
